45b59a6495c676c09b380a3df3c4852be6a34f46
DenisLK <Denul73@gmail.com>
2023-02-08 12:49:05 +0300

    ok

---
+++ jss1/script.js
@@ -0,0 +1,8 @@
+let tempCelc = 25;
+let tempFar = (9/5) * tempCelc + 32;
+console.log(`темпратура в цельсиях: ${tempCelc}
+температура в фаренгейтах: ${tempFar}`);
+
+let firstName = 'Denis';
+let admin = firstName;
+console.log(admin);

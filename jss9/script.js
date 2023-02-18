b749ca28120e3061d7a9c7851aab97098a14ee5e
DenisLK <Denul73@gmail.com>
2023-02-18 13:53:18 +0300

    ok

---
+++ jss9/script.js
@@ -0,0 +1,48 @@
+// 1. При изменении значения в input с id = "from",
+//  значение содержащееся в нем должно моментально отображаться в span.
+//  То есть при печати в input'е тег span также должен меняться.
+
+const spanEl = document.querySelector('span')
+document.querySelector('input').addEventListener('input', e => {
+    spanEl.textContent = e.target.value
+});
+
+
+// 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
+// - добавить два класса: animate_animated и animate_fadeInLeftBig
+// - поставить данному элементу стиль visibility в значение 'visible'.
+
+const messageEl = document.querySelector('.message')
+document.querySelector('button').addEventListener('click', e => {
+    messageEl.classList.add('animate_animated', 'animate_fadeInLeftBig')
+    messageEl.style.visibility = visible
+});
+
+
+// 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме.
+// Если какое - либо поле не заполнено, форма не должна отправляться, 
+// также должны быть подсвечены незаполненные поля(необходимо поставить 
+// класс error незаполненным полям).Как только пользователь начинает заполнять 
+// какое - либо поле, необходимо, при вводе в данное поле, произвести проверку:
+// - Если поле пустое, необходимо данное поле подсветить(поставить класс error данному полю).
+// - Если поле было чем - либо заполнено, подсветку(класс error) необходимо убрать.
+const formEl = document.querySelector('form');
+const form_controlEl = formEl.querySelectorAll('input, select');
+formEl.addEventListener('submit', event => {
+    form_controlEl.forEach(form_controlEl => {
+        if (form_controlEl.value === '') {
+            form_controlEl.classList.add('error');
+            event.preventDefault();
+        }
+    });
+});
+
+formEl.addEventListener('input', event => {
+    if (!event.target.classList.contains('form-control')) {
+        return;
+    }
+    event.target.value === ''
+        ? event.target.classList.add('error')
+        : event.target.classList.remove('error');
+});
+
